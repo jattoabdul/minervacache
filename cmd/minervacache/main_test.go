1c840c2de7856c1748f298abf40a3adcccf0a68b
jattoabdul <jattoade@gmail.com>
2025-04-07 05:12:51 -0400

    update: Plan testing for the main CLI app.

---
+++ cmd/minervacache/main_test.go
@@ -0,0 +1,9 @@
+package main
+
+import "testing"
+
+// TestMinervaCache tests the MinervaCache functionality.
+func TestGRPCIntegration(t *testing.T) {}
+
+// TestHTTPIntegration tests the HTTP integration of the cache.
+func TestHTTPIntegration(t *testing.T) {}
